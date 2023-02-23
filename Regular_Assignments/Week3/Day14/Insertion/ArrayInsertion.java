232b13d80737ca4185010f4d6aa6ed1b1b820837
MahammadSaheed <saheedmdv2133@gmail.com>
2023-02-23 21:57:43 +0530

    done

---
+++ Regular_Assignments/Week3/Day14/Insertion/ArrayInsertion.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class ArrayInsertion{
+  public static void main(String[] args){
+    int n,pos,e;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter length of an array:");
+    n = sc.nextInt();
+    int a[] = new int[n+1];
+    System.out.println("Enter "+n+" all the elements:");
+    for(int i = 0; i < n; i++)
+    {
+      a[i] = sc.nextInt();
+    }
+    System.out.print("Enter the index value:");
+    pos = sc.nextInt();
+    System.out.print("Enter the element which you want to insert:");
+    e = sc.nextInt();
+    for(int i = n-1; i>=pos; i--)
+    {
+      a[i+1] = a[i];
+    }
+    a[pos] = e;
+    System.out.println("After inserting : ");
+    for(int i = 0; i <=n; i++)
+    {
+      System.out.println("a["+i+"]="+a[i]);
+    }
+  }
+}
